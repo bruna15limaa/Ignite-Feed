60937d3fa0e1b8fbdf1288ccb6d95f1024282239
bruna15limaa <97604487+bruna15limaa@users.noreply.github.com>
2024-01-30 14:47:06 -0300

    commentlist

--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -19,7 +20,11 @@ export function Post() {
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
         <p>👉<a href="">jane.design/doctorcare</a></p>
-        <p><a href="">#novoprojeto #nlw #rocketseat</a></p>
+        <p>
+        <a href="">#novoprojeto </a>
+        <a href=""> #nlw </a>
+        <a href=""> #rocketseat</a>
+        </p>
       </div>
       <form className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
@@ -32,6 +37,11 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }
